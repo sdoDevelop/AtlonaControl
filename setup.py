d7264a611da732d56f49180f5e33312d70945aed
Sean O'Connell <sdoconnell92@gmail.com>
2022-11-16 22:57:17 -0600

    Upload

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='AtlonaControl',
-    version='0.8',
+    version='0.9',
     license='MIT',
     author="sdoDevelop",
     author_email='sdoconnell92@gmail.com',
